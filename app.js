fbf7aa8b085aec446a114e4d7a80cf5687ef9f37
sadafshahab12~ <sadafshahab123@yahoo.com>
2024-10-22 02:18:45 +0500

    password checker

---
+++ app.js
@@ -0,0 +1,27 @@
+function checkPassword() {
+  const msg = document.getElementById("message");
+  const minLength = 8;
+  let password = document.getElementById("password").value;
+
+  msg.textContent = "";
+
+  if (password.length < minLength) {
+    msg.textContent = `Password must be at least ${minLength} characters long.`;
+    msg.className = "message invalid";
+    return;
+  }
+
+  const hasUppercase = /[A-Z]/.test(password);
+  const hasLowercase = /[a-z]/.test(password);
+  const hasNumbers = /[0-9]/.test(password);
+  const hasSpecialChars = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+  if (hasUppercase && hasLowercase && hasNumbers && hasSpecialChars) {
+    msg.textContent = "Password is strong.";
+    msg.className = "message valid";
+  } else {
+    msg.textContent =
+      "Password must include uppercase letters, lowercase letters, numbers, and special characters.";
+    msg.className = "message invalid";
+  }
+}
